d45200c519e7b0a59e4c4bb3608c8fe01bb15f20
Psyon <sayan.h2020@gmail.com>
2024-03-02 11:31:16 +0330

    create index

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send({message: 'this is home page'})
+});
+
+app.get('/about', (req,res)=>{
+    res.send({message: 'this is about page'})
+})
+
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log('app is listening...');
+});
